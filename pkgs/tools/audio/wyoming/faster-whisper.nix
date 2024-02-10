41c82c8deda4172729bacee55c090d2df28522b1
Morgan Helton <mhelton@gmail.com>
2024-02-09 20:46:20 -0600

    wyoming-faster-whisper: 1.0.2 -> 1.1.0

--- pkgs/tools/audio/wyoming/faster-whisper.nix
+++ pkgs/tools/audio/wyoming/faster-whisper.nix
@@ -1,34 +1,19 @@
 { lib
 , python3
 , fetchFromGitHub
-, fetchpatch
 }:
 
 python3.pkgs.buildPythonApplication rec {
   pname = "wyoming-faster-whisper";
-  version = "1.0.2";
+  version = "1.1.0";
   pyproject = true;
 
   src = fetchFromGitHub {
     owner = "rhasspy";
     repo = "wyoming-faster-whisper";
     rev = "refs/tags/v${version}";
-    hash = "sha256-mKnWab3i6lEnCBbO3ucNmWIxaaWwQagzfDhaD1U3qow=";
+    hash = "sha256-RD6J/Q7kvd+sgTpR6ERyV+D8gpm0fF38L3U/Jp7gOgk=";
   };
-
-  patches = [
-    # add wyoming-faster-whisper executable
-    (fetchpatch {
-      url = "https://github.com/rhasspy/wyoming-faster-whisper/commit/a5715197abab34253d2864ed8cf406210834b4ec.patch";
-      hash = "sha256-a9gmXMngwXo9ZJDbxl/pPzm6WSy5XeGbz/Xncj7bOog=";
-    })
-
-    # fix model retrieval on python3.11+
-    (fetchpatch {
-      url = "https://github.com/rhasspy/wyoming-faster-whisper/commit/d5229df2c3af536013bc931c1ed7cc239b618208.patch";
-      hash = "sha256-CMpOJ1qSPcdtX2h2ecGmQ/haus/gaSH8r/PCFsMChRY=";
-    })
-  ];
 
   nativeBuildInputs = with python3.pkgs; [
     setuptools
@@ -39,6 +24,11 @@ python3.pkgs.buildPythonApplication rec {
     tokenizers
     wyoming
   ];
+
+  postPatch = ''
+    substituteInPlace setup.py --replace \
+      "name=module_dir" 'name="wyoming_faster_whisper"'
+  '';
 
   pythonImportsCheck = [
     "wyoming_faster_whisper"
